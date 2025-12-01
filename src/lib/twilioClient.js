761faae707cec1035d2a2be94d65924e5a9c8c43
Amitkr77 <amitroyk99@gmail.com>
2025-12-01 16:52:54 +0530

    added twilio client for notification

---
+++ src/lib/twilioClient.js
@@ -0,0 +1,9 @@
+// lib/twilioClient.js
+import twilio from 'twilio';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = twilio(accountSid, authToken);
+
+export default client;
